3c2771fb0daa1f10c7e3da942babf7d897b1c898
yogesh9630 <yogesh93400@gmail.com>
2022-03-09 12:14:14 +0530

    Dot Net Project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnetProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
